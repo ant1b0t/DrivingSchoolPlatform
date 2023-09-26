2724554ea2f94dacd607be66e9c4f2469edd35df
Александр <ant1b0t@yandex.ru>
2023-09-26 15:46:50 +0300

    Add project files.

---
+++ DrivingSchoolPlatform/DrivingSchoolPlatform/Program.cs
@@ -0,0 +1,44 @@
+
+using DrivingSchoolPlatform.Client.Pages;
+using DrivingSchoolPlatform.Components;
+
+namespace DrivingSchoolPlatform
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddServerComponents()
+                .AddWebAssemblyComponents();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.MapRazorComponents<App>()
+              .AddServerRenderMode()
+              .AddWebAssemblyRenderMode()
+              .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+            app.Run();
+        }
+    }
+}
